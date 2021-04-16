b91be5221d6941fa1cc14b8f519361b8ea2234a1
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-16 16:40:07 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// const about = React.createElement("h1", {}, "An Awesome Person");
+// const paragraph = React.createElement('p', {}, "Who is learning React");
+// const list = React.createElement("ul", { className: "my-interests"},
+//   [
+//     React.createElement("li", {}, "JavaScript"),
+//     React.createElement("li", {}, "React"),
+//     React.createElement("li", {}, "Ice cream")
+//   ]
+// );
+
+// const meInReact = React.createElement("div", { className: "me" }, [about, paragraph, list]);
+
+const div = 
+  React.createElement("div", { className: "me" },
+    React.createElement("h1", {}, "An Awesome Person"),
+    React.createElement('p', {}, "Who is learning React"),
+    React.createElement("ul", { className: "my-interests" },
+      [
+        React.createElement("li", {}, "JavaScript"),
+        React.createElement("li", {}, "React"),
+        React.createElement("li", {}, "Movies"),
+        React.createElement("li", {}, "Ice cream")
+      ]
+    )
+  );
+
+const meInReact = div;
+
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+
 ReactDOM.render(
   meInReact, 
   document.getElementById('root')
 );
 
 export default meInReact
-*/
